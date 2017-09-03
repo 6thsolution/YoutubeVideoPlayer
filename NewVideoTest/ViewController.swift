2b710b7270021f9264c909b433ed01c4813ae8d0
mehdi sohrabi <mehdok@gmail.com>
2017-09-03 15:00:12 +0430

    Initial Commit

---
+++ NewVideoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewVideoTest
+//
+//  Created by papyrus on 9/3/17.
+//  Copyright © 2017 SixthSolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
