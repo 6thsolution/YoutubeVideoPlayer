54c697d6b14249715c2750e7b2b309d4a0cef3be
mehdi sohrabi <mehdok@gmail.com>
2017-09-13 16:16:55 +0430

    break point

--- NewVideoTest/RangeLooper/RangePlayerVC.swift
+++ NewVideoTest/RangeLooper/RangePlayerVC.swift
@@ -19,23 +19,23 @@ class RangePlayerVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        XCDYouTubeClient.default().getVideoWithIdentifier("186oNNE6LFM") { (video, error) in
-//            if let streamUrl = (video?.streamURLs[self.small] ??
-//                video?.streamURLs[self.medium] ??
-//                video?.streamURLs[self.hd]){
-//                print("streamUrl", streamUrl.absoluteString)
-//                DispatchQueue.main.async {
-//                    let looper = RangeLooper(videoURL: streamUrl, loopCount: -1)
-//                    looper.start(in: self.view.layer)
-//                }
-//                
-//            }
-//        }
-        looper = RangeLooper(videoURL: URL(string: "https://r3---sn-ab5l6n67.googlevideo.com/videoplayback?ipbits=0&signature=9B02D0998AD1B03F3F154A9FB6C97EF82F9E888B.46C978EFEE4691E6580D5F353CD415729B878930&pl=26&ip=136.0.98.84&mm=31&mn=sn-ab5l6n67&itag=36&ms=au&source=youtube&mv=u&dur=367.293&id=o-AN74uKHUeaD0aaidxltsAvEXQuR9dD2bgXcIJtoYV3BO&clen=10728358&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cexpire&lmt=1503546064583560&gir=yes&mime=video%2F3gpp&requiressl=yes&ei=Lc24WYiXGcal8gS65ZvIAQ&mt=1505283040&key=yt6&expire=1505304973&ratebypass=yes")!, loopCount: -1)
+        XCDYouTubeClient.default().getVideoWithIdentifier("186oNNE6LFM") { (video, error) in
+            if let streamUrl = (video?.streamURLs[self.small] ??
+                video?.streamURLs[self.medium] ??
+                video?.streamURLs[self.hd]){
+                print("streamUrl", streamUrl.absoluteString)
+                DispatchQueue.main.async {
+                    self.looper = RangeLooper(videoURL: streamUrl, loopCount: -1)
+                    self.looper?.start(in: self.view.layer)
+                }
+                
+            }
+        }
+//        looper = RangeLooper(videoURL: URL(string: "https://r3---sn-ab5l6n67.googlevideo.com/videoplayback?ipbits=0&signature=9B02D0998AD1B03F3F154A9FB6C97EF82F9E888B.46C978EFEE4691E6580D5F353CD415729B878930&pl=26&ip=136.0.98.84&mm=31&mn=sn-ab5l6n67&itag=36&ms=au&source=youtube&mv=u&dur=367.293&id=o-AN74uKHUeaD0aaidxltsAvEXQuR9dD2bgXcIJtoYV3BO&clen=10728358&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cexpire&lmt=1503546064583560&gir=yes&mime=video%2F3gpp&requiressl=yes&ei=Lc24WYiXGcal8gS65ZvIAQ&mt=1505283040&key=yt6&expire=1505304973&ratebypass=yes")!, loopCount: -1)
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        looper?.start(in: self.view.layer)
+        //looper?.start(in: self.view.layer)
     }
 
     override func didReceiveMemoryWarning() {
